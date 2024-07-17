c08f82a439201d4e3f4de19b8879537e7199e977
Alpaca <mihaililyin08@gmail.com>
2024-07-17 22:43:12 +0300

    trippyPon

---
+++ Assets/Scripts/CameraSwaper.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwaper : MonoBehaviour
+{
+    public GameObject inactiveCamera;
+    public void Swap()
+    {
+        GameObject player1 = Camera.main.GetComponent<playerFollow>().player.gameObject;
+        GameObject player2 = inactiveCamera.GetComponent<playerFollow>().player.gameObject;
+
+        Camera.main.transform.position = player2.transform.position;
+        Camera.main.GetComponent<playerFollow>().player = player2.transform;
+
+        inactiveCamera.transform.position = player1.transform.position;
+        inactiveCamera.GetComponent<playerFollow>().player = player1.transform;
+
+        player1.GetComponent<PlayerScript>().selected = false;
+        player2.GetComponent<PlayerScript>().selected = true;
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -89,16 +89,11 @@ public class PlayerScript : MonoBehaviour
         {
             if (!sit)
             {
-                float dirX = Input.GetAxis("Vertical");
-                if (Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position) > 0.6f || dirX < 0)
-                {
-                    rb.velocity = transform.up * dirX * speed;
-                    LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-                }
-                else
-                {
-                    rb.velocity = Vector2.zero;
-                }
+                float dirX = Input.GetAxis("Horizontal");
+                float dirY = Input.GetAxis("Vertical");
+
+                rb.velocity = new Vector2(dirX, dirY) * speed;
+                LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             }
 
         }
