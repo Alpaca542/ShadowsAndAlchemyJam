a46535df9d199c93e7a9d0abebc6da803543c009
Areeoff <mailtoareeoff@gmail.com>
2024-07-18 22:51:47 +0500

    Boiler

--- Assets/BoilerScript.cs
+++ Assets/BoilerScript.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class shrederSctipt : MonoBehaviour
+public class BoilerScript : MonoBehaviour
 {
+    public GameObject indicator;
+    public GameObject requirer;
+    public GameObject requirer1;
     // Start is called before the first frame update
+    void Start()
+    {
+        indicator.GetComponent<Image>().color = Color.red;
+    }
+
+    // Update is called once per frame
     public GameObject shrederUI;
-    public GameObject[] spawnPoints;
     bool interact = false;
     public GameObject tube;
     Collision2D collision;
     public BoilerScript boiler;
-    public GameObject redMatter;
-    public GameObject Impure;
     bool check = false;
-    void Start()
-    {
-        
-    }
+
+    int blueNum = 0;
+
     void Check()
     {
         int k = 0;
@@ -32,9 +38,9 @@ public class shrederSctipt : MonoBehaviour
         if ((k == 0))
         {
 
-            shrederUI.SetActive(false);
+            //shrederUI.SetActive(false);
             tube.GetComponent<SpriteRenderer>().color = Color.red;
-            Invoke(nameof(turnTube),1f);
+            Invoke(nameof(turnTube), 1f);
             collision.gameObject.GetComponent<CookScript>().UnFreeze();
             check = false;
         }
@@ -53,43 +59,38 @@ public class shrederSctipt : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                
-                shrederUI.SetActive(true);
-                collision.gameObject.GetComponent<CookScript>().Freeze();
-                ClearMePlease();
-                collision.gameObject.GetComponent<CookScript>().WhatIHave = "0";
-                interact = false;
-                check = true;
-            }
-            
-        }
-        if(check) { Check(); }
-        
-    }
-    public void ClearMePlease()
-    {
-        foreach (GameObject go in spawnPoints)
-        {
-            if ((Random.Range(0, 3) == 0)|| (Random.Range(0, 3) == 1))
-            {
-                if ((Random.Range(0, 2) == 0))
+
+                //shrederUI.SetActive(true);
+                //collision.gameObject.GetComponent<CookScript>().Freeze();
+                if ((collision.gameObject.GetComponent<CookScript>().WhatIHave == "blue"))
                 {
-                    Instantiate(Impure, go.transform.position,Quaternion.identity, shrederUI.transform);
-                }  
-                else
+                    blueNum += 1;
+                    collision.gameObject.GetComponent<CookScript>().WhatIHave = "0";
+                }
+                if(blueNum ==1)
                 {
-                    Instantiate(redMatter, go.transform.position, Quaternion.identity, shrederUI.transform);
+                    requirer1.gameObject.GetComponent<Image>().color = Color.blue;
+                }
+                if (blueNum == 2)
+                {
+                    requirer.gameObject.GetComponent<Image>().color = Color.blue;
+                }
+                if(blueNum > 2)
+                {
+                    turnTube();
                 }
             }
+
         }
-        
+       
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Cook"))
         {
-            
-            if ((collision.gameObject.GetComponent<CookScript>().WhatIHave == "red"))
+
+            if ((collision.gameObject.GetComponent<CookScript>().WhatIHave == "blue"))
             {
                 interact = true;
                 this.collision = collision;
@@ -101,9 +102,9 @@ public class shrederSctipt : MonoBehaviour
         if (collision.gameObject.CompareTag("Cook"))
         {
 
-            
-                collision.gameObject.GetComponent<CookScript>().UnFreeze();
-            
+
+            interact = false;
+
         }
     }
 }
