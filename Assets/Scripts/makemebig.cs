32939f01b9dac86bd8cdaee83357a9ec26617222
Alpaca <mihaililyin08@gmail.com>
2024-07-27 22:38:54 +0300

    betterShape

--- Assets/Scripts/makemebig.cs
+++ Assets/Scripts/makemebig.cs
@@ -1,11 +1,13 @@
 using AirFishLab.ScrollingList;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class makemebig : MonoBehaviour
 {
     public void OnFocusingBoxChanged(ListBox prevFocusingBox, ListBox curFocusingBox)
     {
         curFocusingBox.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(160f, 160f);
+        curFocusingBox.gameObject.GetComponent<Button>().onClick.Invoke();
         if (prevFocusingBox != null)
         {
             prevFocusingBox.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(90.8f, 93.9f);
