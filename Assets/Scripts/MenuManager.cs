11414ad6f8e3e4ff7b62957a9c93a01bb498bc4a
Alpaca <mihaililyin08@gmail.com>
2024-07-28 19:20:24 +0300

    new scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject SmokeEffect;
+    public GameObject EndSmoke;
+
+    public GameObject settingsPanel;
+    public GameObject menuPanel;
+    public void OnStartClicked()
+    {
+        SmokeEffect.SetActive(true);
+        Invoke(nameof(InvokeOpenLevel), 1f);
+    }
+    public void InvokeOpenLevel()
+    {
+        SceneManager.LoadScene("GameScene1");
+    }
+    private void Start()
+    {
+        EndSmoke.SetActive(true);
+        if (!PlayerPrefs.HasKey("Started") && SceneManager.GetActiveScene().name == "Menu")
+        {
+            EndSmoke.SetActive(false);
+            PlayerPrefs.SetInt("Started", 1);
+        }
+        else
+        {
+            Invoke(nameof(InvokeSmokeStop), 3f);
+        }
+    }
+    public void InvokeSmokeStop()
+    {
+        EndSmoke.SetActive(false);
+    }
+    public void OpenSetting()
+    {
+        settingsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+}
